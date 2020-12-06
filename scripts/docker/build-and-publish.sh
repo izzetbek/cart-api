9c2c30b898c8b5634dccf5de02e5ba055fbd0455
Izzat_Fatullayev <Izzat_Fatullayev@epam.com>
2020-12-06 21:12:27 +0300

    feat: Updated deploy config

--- scripts/docker/build-and-publish.sh
+++ scripts/docker/build-and-publish.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-REGION=us-west-1
-AWS_ACCOUNT_ID=
+REGION=eu-west-1
+AWS_ACCOUNT_ID=119595940820
 PROFILE=default
 #PROFILE=personalAccount
-DOCKER_APP_NAME=my-cart-api
 
-DOCKER_APP_REPOSITORY=my-cart-api
+DOCKER_APP_REPOSITORY=izzetbek-cart-api
 AWS_DOCKER_REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 DOCKER_TIME_TAG="$(date +%s)"
 DOCKER_LATEST_TAG=latest
 
 # Logout from Docker
-docker logout
+docker logout $AWS_DOCKER_REGISTRY_URL
 # Login in AWS container registry with your AWS credentials
 aws ecr get-login-password --profile $PROFILE --region $REGION | docker login --username AWS --password-stdin $AWS_DOCKER_REGISTRY_URL
 # Build Docker image
-docker build -f "$(dirname "$0")/../../Dockerfiles/Dockerfile" -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
+docker build \
+        -f "$(dirname "$0")/../../Dockerfiles/Dockerfile" \
+        -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" \
+        -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
 
 # Tag Docker image
 docker tag $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" $AWS_DOCKER_REGISTRY_URL/$DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG"
--- scripts/docker/deploy.sh
+++ scripts/docker/deploy.sh
@@ -3,10 +3,10 @@
 #ENV=development
 ENV=develop
 
-REGION=eu-central-1
-AWS_ACCOUNT_ID=724443507988
-#PROFILE=default
-PROFILE=personalAccount
+REGION=eu-west-1
+AWS_ACCOUNT_ID=119595940820
+PROFILE=default
+#PROFILE=personalAccount
 
 AWS_DOCKER_REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
